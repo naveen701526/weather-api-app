9e5b49f43efa086d732c6c4ca3845b09e8e5f23b
naveen <neonew8984@gmail.com>
2021-05-31 20:35:30 +0530

    weather app

--- js/api.js
+++ js/api.js
@@ -1,7 +1,29 @@
 const key = 'PMzDXDQFErGEeGQ8gwmpwd0wqYEZI9Ni';
 
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    // return data[0];
+    console.log(data);
+};
+
 const getCityInfo = async (cityName) => {
     const base =
         'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
+    const query = `?apikey=${key}&q=${cityName}`;
+
+    const response = await fetch(base + query);
+
+    const data = await response.json();
+
+    // return data[0];
+    console.log(data);
 };
+
+// 204842
+
+getWeather(204842);
