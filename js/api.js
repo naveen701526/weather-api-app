c61c74ba5f00505df9a0e024b5bd97d0a5ffa801
naveen <neonew8984@gmail.com>
2021-05-31 20:28:51 +0530

    weather app

---
+++ js/api.js
@@ -0,0 +1,7 @@
+const key = 'PMzDXDQFErGEeGQ8gwmpwd0wqYEZI9Ni';
+
+const getCityInfo = async (cityName) => {
+    const base =
+        'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+};
