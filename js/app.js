d04ad0a275e2b9a2c5aeae141cbef2b44d9c9b4e
naveen <neonew8984@gmail.com>
2021-05-31 20:38:13 +0530

    weather app

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+
+const updateCity = async (city) => {
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    return {cityDets, weather};
+};
+
+cityForm.addEventListener('submit', (e) => {
+    // prevent default action
+    e.preventDefault();
+
+    // // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with new city
+    updateCity(city)
+        .then((data) => updateUI(data))
+        .catch((err) => console.log(err));
+});
